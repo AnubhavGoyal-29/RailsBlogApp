f5268d958fa7b2448425a8b029b0081b4d998a41
AnubhavGoyal-29 <anubhavgoyal2910@gmail.com>
2022-01-19 20:51:46 +0530

    deleted maximum files

--- simple-blog/test/controllers/nuser_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class NuserControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- simple-blog/test/models/nuser_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class NuserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
